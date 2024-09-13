0645f930093f97f810f4ed04289d292b101484e0
Raijohn <166853579+Raijohn@users.noreply.github.com>
2024-09-13 18:20:38 +0200

    Tree_Travesal.py

--- Tree_Travesal.py
+++ Tree_Travesal.py
@@ -3,13 +3,29 @@ class Node:
         self.key = key
         self.left = None
         self.right = None
-
+#lpr
 def in_order_traversal(root):
-    while root.left != None:
+    if root.left != None:
         in_order_traversal(root.left)
     print(root.key)
-    while root.right != None:
+    if root.right != None:
         in_order_traversal(root.right)
+
+#plr
+def pre_order_traversal(root):
+    print(root.key)
+    if root.left != None:
+        pre_order_traversal(root.left)
+    if root.right != None:
+        pre_order_traversal(root.right)
+
+#lrp
+def post_order_traversal(root):
+    if root.left != None:
+        post_order_traversal(root.left)
+    if root.right != None:
+        post_order_traversal(root.right)
+    print(root.key)
 
 Root = Node(1)
 
@@ -22,4 +38,8 @@ Root.left.right = Node(5)
 Root.right.left = Node(6)
 Root.right.right = Node(7)
 
+pre_order_traversal(Root)
+print(" ")
+post_order_traversal(Root)
+print(" ")
 in_order_traversal(Root)
