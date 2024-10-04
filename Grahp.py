7dc9b447c74d3abc5d8cde0418de82f5de2ce235
Raijohn <166853579+Raijohn@users.noreply.github.com>
2024-10-04 18:50:26 +0200

    Grahp.py

--- Grahp.py
+++ Grahp.py
@@ -1,4 +1,5 @@
 import queue_class
+import stack
 class Graph:
     def __init__(self,nodes):
         self.nodes = nodes
@@ -24,6 +25,21 @@ class Graph:
                     visited.append(n)
                     queue.enqueue(n)
         print(visited)
+    
+    def DFS(self,start):
+        stack1 = stack.Stack(self.nodes)
+        visited = []
+        stack1.push(start)
+        while stack1.isEmpty() == False:
+            add_visited = stack1.pop()
+            visited.append(add_visited)
+            for node in self.adjacencylist[add_visited]:
+                if node not in visited and not stack1.check_is_there(node):
+                    stack1.push(node)
+        print(visited)
+
+
+
 
 graph1 = Graph(9)
 graph1.add_edge(0,1)
@@ -37,4 +53,6 @@ graph1.add_edge(6,7)
 graph1.add_edge(5,8)
 graph1.add_edge(7,8)
 
+graph1.BFS(0)
+graph1.DFS(0)
 graph1.BFS(0)
